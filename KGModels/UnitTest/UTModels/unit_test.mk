8ecd19256b0d34bed683a14f2563e7f1de7dc241
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-09-04 15:55:32 +0900

    test: Add "Check" target for UTModels unit test

---
+++ KGModels/UnitTest/UTModels/unit_test.mk
@@ -0,0 +1,19 @@
+#
+# unittest.mk
+#
+
+TEST_NAME	= UTModels
+
+unit_test	= $(BUILD_DIR)/$(CONFIGURATION)/$(TEST_NAME)
+test_dir	= ../UnitTest/$(TEST_NAME)
+test_log	= $(BUILD_DIR)/$(CONFIGURATION)/$(TEST_NAME).log
+
+unit_test: dummy
+	test -f $(unit_test)
+	(cd $(test_dir) ; \
+	 $(unit_test) 2>&1 | grep -v "Which one is undefined" > $(test_log) \
+	)
+	diff $(test_log) $(test_dir)/$(TEST_NAME).log.OK
+
+dummy:
+
