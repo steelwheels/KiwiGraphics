67e02e4fc6c4bf14b1a16006bc24b644765ac230
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-09-04 12:37:48 +0900

    test: Add UTModels console application for unit test

---
+++ KGModels/UnitTest/UTModels/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UTModels
+//
+//  Created by Tomoo Hamada on 2016/09/04.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
