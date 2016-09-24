c1ac08f9aabe8a3204fef712e041f210418c5285
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-09-25 00:29:17 +0900

    KGImage: Extend NSImage and CGImage

---
+++ Source/Graphics/KGImage.swift
@@ -0,0 +1,61 @@
+/**
+ * @file	KGImage.swift
+ * @brief	Extend KGImage class
+ * @par Copyright
+ *   Copyright (C) 2016 Steel Wheels Project
+ * @par Reference
+ *    http://qiita.com/HaNoHito/items/2fe95aba853f9cedcd3e
+ */
+
+import Foundation
+
+#if os(iOS)
+#else
+import Cocoa
+#endif
+
+#if os(iOS)
+	public typealias KGImage = UIImage
+#else
+	public typealias KGImage = NSImage
+#endif
+
+#if os(OSX)
+
+extension NSImage {
+	public var toCGImage: CGImage {
+		var imageRect = NSRect(x: 0, y: 0, width: size.width, height: size.height)
+		#if swift(>=3.0)
+			guard let image = cgImage(forProposedRect: &imageRect, context: nil, hints: nil) else {
+				abort()
+			}
+		#else
+			guard let image = CGImageForProposedRect(&imageRect, context: nil, hints: nil) else {
+				abort()
+			}
+		#endif
+		return image
+	}
+}
+
+extension CGImage {
+	public var size: CGSize {
+		#if swift(>=3.0)
+		#else
+			let width  = CGImageGetWidth(self)
+			let height = CGImageGetHeight(self)
+		#endif
+		return CGSize(width: width, height: height)
+	}
+
+	public var toNSImage: NSImage {
+		#if swift(>=3.0)
+			return NSImage(cgImage: self, size: size)
+		#else
+			return NSImage(CGImage: self, size: size)
+		#endif
+	}
+}
+
+#endif
+
